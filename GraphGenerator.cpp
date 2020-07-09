98cdbed43eafaf6c76d2613470f9f3a8cbc9cca0
Carlos Henrique Stapait Junior <chsjwarrior@gmail.com>
2020-07-08 22:10:17 -0300

    correções

--- GraphGenerator.cpp
+++ GraphGenerator.cpp
@@ -60,6 +60,12 @@ std::list<Edge>* simpleRandow(const unsigned int& amountVertices, const bool& is
 	return edges;
 }
 
+std::list<Edge>* randowByDegree(const unsigned int& amountVertices, const bool& isDigraph, const int& maxValue, const int& minValue, const std::vector<unsigned int>& inDegree, const std::vector<unsigned int>& outDegree) {
+
+
+
+}
+
 std::list<Edge>* completeRegular(const unsigned int& amountVertices, const bool& isDigraph, const int& maxValue, const int& minValue) {
 	std::list<Edge>* edges = new std::list<Edge>();
 
@@ -83,15 +89,47 @@ std::list<Edge>* completeBipartite(const unsigned int& amountVerticesA, const un
 	return edges;
 }
 
+template<typename T>
+int signedRead(const T arg, const int& min, const int& max) {
+	int value = NULL;
+	do {
+		std::cout << arg << std::endl;
+		std::cin >> value;
+		if (std::cin.fail()) {
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			system("CLS");
+			value = min;
+		}
+	} while (value < min || value > max);
+	return value;
+}
+
+template<typename T>
+unsigned int unsignedRead(const T arg, const unsigned int& min, const unsigned int& max) {
+	unsigned int value = NULL;
+	do {
+		std::cout << arg << std::endl;
+		std::cin >> value;
+		if (std::cin.fail()) {
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			system("CLS");
+			value = max;
+		}
+	} while (value < min || value > max);
+	return value;
+}
+
 int main() {
 	srand(time(NULL));
 
 	int value = 0;
 	do {
-		std::cout << "Dgt 1: Novo Grafo aleatorio" << std::endl;
-		std::cout << "Dgt 2: Novo Grafo aleatorio simples" << std::endl;
-		std::cout << "Dgt 3: Novo grafo regular completo" << std::endl;
-		std::cout << "Dgt 4: Novo grafo bipartido completo" << std::endl;
+		std::cout << "Dgt 1: Grafo aleatorio" << std::endl;
+		std::cout << "Dgt 2: Grafo aleatorio simples" << std::endl;
+		std::cout << "Dgt 3: Grafo regular completo" << std::endl;
+		std::cout << "Dgt 4: Grafo bipartido completo" << std::endl;
 		std::cout << "Dgt 0: Encerrar." << std::endl;
 		std::cin >> value;
 	} while (value < 0 || value > 4);
@@ -105,43 +143,29 @@ int main() {
 		bool isDigraph = isValued;
 
 		if (value == 4) {
-			std::cout << "Dgt a qtd de Vertices do conjunto 1." << std::endl;
-			unsigned int amountVerticesA;
-			std::cin >> amountVerticesA;
+			unsigned int amountVerticesA = unsignedRead("Dgt a qtd de Vertices do conjunto 1.", 3, UINT_MAX - 1);
 
-			std::cout << "Dgt a qtd de Vertices do conjunto 2." << std::endl;
-			unsigned int amountVerticesB;
-			std::cin >> amountVerticesB;
+			unsigned int amountVerticesB = unsignedRead("Dgt a qtd de Vertices do conjunto 2.", 3, UINT_MAX - 1);
 
-			std::cout << "Dgt 0: Grafo nao valorado.\nDgt qualquer valor : Grafo valorado." << std::endl;
-			std::cin >> isValued;
+			isValued = signedRead("Dgt 0: Grafo nao valorado.\nDgt qualquer valor : Grafo valorado.", INT_MIN, INT_MAX);
 			if (isValued) {
-				std::cout << "Dgt o maior valor do Grafo valorado." << std::endl;
-				std::cin >> maxValue;
-				std::cout << "Dgt o menor valor do Grafo valorado." << std::endl;
-				std::cin >> minValue;
+				maxValue = signedRead("Dgt o maior valor do Grafo valorado.", INT_MIN + 1, INT_MAX);
+				minValue = signedRead("Dgt o menor valor do Grafo valorado.", INT_MIN, maxValue);
 			}
-
-			isDigraph = false;
 
 			edges = completeBipartite(amountVerticesA, amountVerticesB, maxValue, minValue);
 
 			amountVertices = amountVerticesA + amountVerticesB;
 		} else {
-			std::cout << "Dgt a qtd de Vertices." << std::endl;
-			std::cin >> amountVertices;
+			amountVertices = unsignedRead("Dgt a qtd de Vertices.", 3, UINT_MAX - 1);
 
-			std::cout << "Dgt 0: Grafo nao valorado.\nDgt qualquer valor : Grafo valorado." << std::endl;
-			std::cin >> isValued;
+			isValued = signedRead("Dgt 0: Grafo nao valorado.\nDgt qualquer valor : Grafo valorado.", INT_MIN, INT_MAX);
 			if (isValued) {
-				std::cout << "Dgt o maior valor do Grafo valorado." << std::endl;
-				std::cin >> maxValue;
-				std::cout << "Dgt o menor valor do Grafo valorado." << std::endl;
-				std::cin >> minValue;
+				maxValue = signedRead("Dgt o maior valor do Grafo valorado.", INT_MIN + 1, INT_MAX);
+				minValue = signedRead("Dgt o menor valor do Grafo valorado.", INT_MIN, maxValue);
 			}
 
-			std::cout << "Dgt 0: Grafo nao dirigido.\nDgt qualquer valor: Grafo dirigido." << std::endl;
-			std::cin >> isDigraph;
+			isDigraph = signedRead("Dgt 0: Grafo nao dirigido.\nDgt qualquer valor: Grafo dirigido.", INT_MIN, INT_MAX);
 
 			if (value == 1 || value == 2) {
 				std::cout << "Dgt a probabilidade de insersao" << std::endl;
@@ -159,10 +183,13 @@ int main() {
 
 		if (edges != nullptr) {
 			std::ofstream file;
-			file.open("Graph.txt");
-			file << amountVertices << " " << edges->size() << " " << (isDigraph ? 1 : 0) << std::endl;
-			for (auto e = edges->cbegin(); e != edges->cend(); e = edges->erase(e))
-				file << e->U << " " << e->V << " " << e->W << std::endl;
+			file.open("Graph.txt", std::ios::out);
+			if (file.is_open()) {
+				file << amountVertices << " " << edges->size() << " " << (isDigraph ? 1 : 0) << std::endl;
+				for (auto e = edges->cbegin(); e != edges->cend(); e = edges->erase(e))
+					file << e->U << " " << e->V << " " << e->W << std::endl;
+			} else
+				std::cerr << "Falhou na criação do arquivo." << std::endl;
 			file.close();
 			delete edges;
 			edges = nullptr;
